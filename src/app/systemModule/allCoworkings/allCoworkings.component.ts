a01da569cf9718f13bc92f7ac2a7608f6171c550
kparamonov <prmnr2k@gmail.com>
2017-11-19 15:26:06 +0300

    saddsasd

--- src/app/systemModule/allCoworkings/allCoworkings.component.ts
+++ src/app/systemModule/allCoworkings/allCoworkings.component.ts
@@ -37,8 +37,8 @@ export class AllCoworkingsComponent implements OnInit {
       begin_work:'',
       end_work:'',
       lat:null,
-      lng:null,
-      date:null
+      lng:null
+      //date:null
     };
     constructor(private service: MainService, private router: Router, private ng2cable: Ng2Cable, private broadcaster: Broadcaster) {
       
